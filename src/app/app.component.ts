cb2a26c173781192949e88e15a04aaea082714b1
phucfeelforfun <banbmt1509@gmail.com>
2023-08-25 09:44:37 +0700

    init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>hello angular</h1>
+  <app-new></app-new>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'test';
+}
